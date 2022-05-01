ce3c03e615a7f4760501862d757d29aa45d46e8c
Serdar <serdar.onculer.work@hotmail.com>
2022-05-02 01:53:57 +0300

    basic adjustments made

---
+++ lib/core/constants/color_constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color black = Colors.black;
+  static const Color white = Colors.white;
+  
+}
---
+++ lib/core/themes/themes.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class MyTheme {
+  static SystemUiOverlayStyle statusBarTheme = const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+    statusBarIconBrightness: Brightness.dark,
+  );
+
+  
+}
